850aca1e40b14d31df101c0b3c15c1fec6463667
Brendan Hasz <winsto99@gmail.com>
2014-08-20 08:22:08 -0500

    added elevation lookup

--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
 # stats.py
+#
 # Functions for calculating statistics of paths and polygons
+#
 # Brendan Hasz
 # winsto99@gmail.com
 # April 2014
@@ -7,8 +9,43 @@
 # TODO: have to convert lat + long to miles: 
 #           one deg of lat = 69.1 mi
 #           one deg of long = 53 mi
+# but depends on lat + long?
+# see http://www.movable-type.co.uk/scripts/latlong.html
 
 from math import sqrt
+
+def elevation(lat, lng):
+    '''
+    Returns the elevation at specified lat and longitude
+    '''
+    def grep(lines, word):
+        ''' Returns only lines containing word '''
+        wordlines = []
+        for l in lines:
+            if word in l:
+                wordlines.append(l)
+        return wordlines
+    def substring(string, a, b):
+        ''' Returns string from end of substr a to start of substr b '''
+        bind = string.find(a)+len(a)
+        eind = string.find(b)
+        return string[bind:eind].strip()
+    import urllib2
+    if not isinstance(lat, basestring):
+        lat = str(lat)
+    if not isinstance(lng, basestring):
+        lng = str(lng)
+    reqstr = "http://maps.googleapis.com/maps/api/elevation/json?locations="
+    resp = urllib2.urlopen(reqstr+lat+','+lng).read().split('\n') #read google elevation api response
+    eleline = grep(resp, "elevation")[0] #find the elevation line
+    return float(substring(eleline, ": ", ',')) #extract the elevation
+
+
+def havdist(a, b):
+    '''
+    Calculates the distance (in mi) between two [lat, lon] pairs
+    '''
+    #R = 
 
 def area(points):
     '''
